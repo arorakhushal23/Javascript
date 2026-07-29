bbdfcfd4cc3c3749f49db654dd9fb564570c2c92
Khushal Arora <arorakhushal23@gmail.com>
2026-07-29 10:18:47 +0530

    Modified

--- Todo_app/script.js
+++ Todo_app/script.js
@@ -4,30 +4,41 @@ const list = document.getElementById("taskList");
 
 let tasks = [];
 
+// Create Search Bar
 
-// Save tasks
+const searchInput = document.createElement("input");
+searchInput.type = "text";
+searchInput.id = "searchInput";
+searchInput.placeholder = "Search Tasks...";
+
+// Insert search bar before task input
+document.body.insertBefore(searchInput, input);
+
+// Save Tasks
+
 function saveTasks() {
     localStorage.setItem("tasks", JSON.stringify(tasks));
 }
 
 // Create Task
+
 function createTask(task) {
 
     const li = document.createElement("li");
 
     const div = document.createElement("div");
     div.className = "task";
-    div.style.border="2px solid black"
+    div.style.border = "2px solid black";
 
     const taskText = document.createElement("span");
     taskText.textContent = task;
-  
 
     div.appendChild(taskText);
-    
+
     // Delete Button
     const deleteButton = document.createElement("button");
     deleteButton.textContent = "Delete";
+    deleteButton.className = "delete-btn";
 
     deleteButton.addEventListener("click", function () {
 
@@ -47,6 +58,7 @@ function createTask(task) {
     // Edit Button
     const editButton = document.createElement("button");
     editButton.textContent = "Edit";
+    editButton.className = "edit-btn";
 
     editButton.addEventListener("click", function () {
 
@@ -68,12 +80,10 @@ function createTask(task) {
     li.appendChild(div);
 
     list.appendChild(li);
-    deleteButton.className = "delete-btn";
-editButton.className = "edit-btn";
 }
 
-
 // Load Tasks
+
 function loadTasks() {
 
     const savedTasks = localStorage.getItem("tasks");
@@ -95,6 +105,7 @@ function loadTasks() {
 loadTasks();
 
 // Add Task
+
 button.addEventListener("click", function () {
 
     const task = input.value.trim();
@@ -110,5 +121,29 @@ button.addEventListener("click", function () {
     createTask(task);
 
     input.value = "";
+
+});
+
+
+// Search Tasks
+
+
+searchInput.addEventListener("input", function () {
+
+    const searchText = searchInput.value.toLowerCase();
+
+    const allTasks = document.querySelectorAll("#taskList li");
+
+    allTasks.forEach(function (task) {
+
+        const taskName = task.querySelector("span").textContent.toLowerCase();
+
+        if (taskName.includes(searchText)) {
+            task.style.display = "";
+        } else {
+            task.style.display = "none";
+        }
+
+    });
 
 });
